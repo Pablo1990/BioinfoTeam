ccbfade7c3ba6f034589b0e73e3c8b70eaeb4019
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-11-24 14:42:44 +0100

    Trying to read a xml from an url

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,20 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+//http://stackoverflow.com/questions/11946153/how-to-parse-this-xml-in-titanium
+//http://docs.appcelerator.com/titanium/latest/#!/api
+
+//Get the xml : https://codejunkster.wordpress.com/2011/06/29/xml-http-requests-httpclient-with-titanium-appcelerator/
+
+ 	/*
+	 * http://hackathonem.com/hackaton.pdf
+	 * URL de acceso
+	 *  http://www.elmundo.es/rss/hackathon/albumes.xml
+	 * http://www.elmundo.es/rss/hackathon/ciencia.xml
+	 * http://www.elmundo.es/rss/hackathon/espana.xml
+	 * http://www.elmundo.es/rss/hackathon/economia.xml
+	 * http://www.elmundo.es/rss/hackathon/internacional.xml
+	 */
+
+//https://github.com/appcelerator/titanium_mobile/blob/master/demos/KitchenSink/Resources/examples/rss.js
