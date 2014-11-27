510f741b0672493bac51a907d2453fbad035d048
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-11-27 13:03:55 +0100

    Reading a xml. And getting the title

--- app/alloy.js
+++ app/alloy.js
@@ -13,16 +13,36 @@
 //http://stackoverflow.com/questions/11946153/how-to-parse-this-xml-in-titanium
 //http://docs.appcelerator.com/titanium/latest/#!/api
 
-//Get the xml : https://codejunkster.wordpress.com/2011/06/29/xml-http-requests-httpclient-with-titanium-appcelerator/
-
- 	/*
-	 * http://hackathonem.com/hackaton.pdf
-	 * URL de acceso
-	 *  http://www.elmundo.es/rss/hackathon/albumes.xml
-	 * http://www.elmundo.es/rss/hackathon/ciencia.xml
-	 * http://www.elmundo.es/rss/hackathon/espana.xml
-	 * http://www.elmundo.es/rss/hackathon/economia.xml
-	 * http://www.elmundo.es/rss/hackathon/internacional.xml
-	 */
+/*
+* http://hackathonem.com/hackaton.pdf
+* URL de acceso
+*  http://www.elmundo.es/rss/hackathon/albumes.xml
+* http://www.elmundo.es/rss/hackathon/ciencia.xml
+* http://www.elmundo.es/rss/hackathon/espana.xml
+* http://www.elmundo.es/rss/hackathon/economia.xml
+* http://www.elmundo.es/rss/hackathon/internacional.xml
+*/
 
 //https://github.com/appcelerator/titanium_mobile/blob/master/demos/KitchenSink/Resources/examples/rss.js
+
+url = 'http://www.elmundo.es/rss/hackathon/ciencia.xml';
+data = [];
+Ti.API.info('>>>> loading RSS feed ' + url);
+xhr = Titanium.Network.createHTTPClient();
+xhr.open('GET', url);
+xhr.onload = function() {
+
+	Ti.API.info('>>> got the feed! ... ');
+
+	// Now parse the feed XML
+	var xml = this.responseXML;
+
+	var itemList = xml.documentElement.getElementsByTagName("item");
+	Ti.API.info('found '+itemList.length+' items in the RSS feed');
+	
+	for (var i=0; i < itemList.length; i++) {
+		Ti.API.info('Item ' + itemList.item(i).getElementsByTagName("title").item(0).textContent);
+	}
+};
+
+xhr.send();
